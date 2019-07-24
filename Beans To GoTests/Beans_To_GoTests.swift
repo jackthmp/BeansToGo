22fd1a3dd6f3c264ef3bfda32784480c3bc472d5
Jack Thompson <jackt@yelp.com>
2019-07-24 15:28:35 -0700

    Initial Commit

---
+++ Beans To GoTests/Beans_To_GoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Beans_To_GoTests.swift
+//  Beans To GoTests
+//
+//  Created by Jack Thompson on 7/24/19.
+//  Copyright © 2019 Jack Thompson. All rights reserved.
+//
+
+import XCTest
+@testable import Beans_To_Go
+
+class Beans_To_GoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
